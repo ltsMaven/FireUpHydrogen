2ebfd2e4b7ff68dcf976aad6cdca055fe81eb3b8
agun0042 <agun0042@student.monash.edu>
2026-03-10 18:47:54 +1100

    change the logo to fire up logo

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ import {
   Await,
 } from 'react-router';
 import type {Route} from './+types/root';
-import favicon from '~/assets/favicon.svg';
+import faviconPng from '~/assets/fireup-logo.png';
 import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
 import resetStyles from '~/styles/reset.css?url';
 import appStyles from '~/styles/app.css?url';
@@ -60,7 +60,7 @@ export function links() {
   return [
     {rel: 'preconnect', href: 'https://cdn.shopify.com'},
     {rel: 'preconnect', href: 'https://shop.app'},
-    {rel: 'icon', type: 'image/svg+xml', href: favicon},
+    {rel: 'icon', type: 'image/png', href: faviconPng},
   ];
 }
 
